64fe7d539fbeec95bf1f46b4f979bed77145a716
bigyidbuilds <bigyidbuilds@gmail.com>
2021-03-31 22:45:45 +0100

    Create uservar.py

---
+++ uservar.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+class tmdbapi():
+	''' TMDB Details key obtainable from https://www.themoviedb.org/settings/api '''
+	key=''
+	username=''
+	password=''
+
+
+class youtubeapi():
+	''' Youtube details https://developers.google.com/youtube/v3/getting-started '''
+	apiKey = ''
+	clientId = ''
+	clientSecret = ''
+
+
+class url():
+	''' url path to your playlist files, film and tv required are on seperate files ''' 
+	scheme   = 'https'
+	netloc   = 'raw.githubusercontent.com'
+	filmpath = 'bigyidbuilds/plugin.video.flixgui.example.basic/20842b0450692c75a9b9559d84ec34677800f4b2/_playlist_example/movies.json'
+	tvpath   = 'bigyidbuilds/plugin.video.flixgui.example.basic/20842b0450692c75a9b9559d84ec34677800f4b2/_playlist_example/tvshows.json'
+
+
+
+class colors():
+	'''Colours for loading screen, use color hex code for colour e.g FFFFFFFF = WHITE FF000000 = BLACK '''
+	colortheme = 'FF00F9FF'
+	backcolor  = 'FF4F5B66'
+	progressbg = 'FFFFFFFF'
+
+
+
